f7ec0ab59740aa8eec7406d1c22c5cce5ae0ce88
Faizan Sana <faizansana01@gmail.com>
2023-08-22 17:06:59 +0000

    Automatically select least used gpu for containers

--- dev_config.sh
+++ dev_config.sh
@@ -21,6 +21,13 @@ FIXGID=$(id -g)
 CARLA_VERSION=${CARLA_VERSION:-"0.9.10.1"}
 CARLA_QUALITY=${CARLA_QUALITY:-"Low"}
 
+## -------------------------- Determine GPU with Lowest Memory Usage ------------------------
+
+# Find the GPU with the lowest memory usage
+GPU_ID_CARLA_MAIN=$(nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | sort -n -k2 | awk -F', ' '{print $1}' | head -n 1)
+# Find GPU with second lowest memory usage
+GPU_ID_CARLA_DEBUG=$(nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | sort -n -k2 | awk -F', ' '{print $1}' | head -n 2 | tail -n 1)
+
 ## -------------------------- CUDA Version ------------------------
 CUDA_VERSION=${CUDA_VERSION:-"11.8.0"}
 
@@ -30,5 +37,8 @@ echo "FIXGID=$FIXGID" >> "$SCRIPT_DIR/.env"
 
 echo "CARLA_VERSION=$CARLA_VERSION" >> "$SCRIPT_DIR/.env"
 echo "CARLA_QUALITY=$CARLA_QUALITY" >> "$SCRIPT_DIR/.env"
+
+echo "GPU_ID_CARLA_MAIN=$GPU_ID_CARLA_MAIN" >> "$SCRIPT_DIR/.env"
+echo "GPU_ID_CARLA_DEBUG=$GPU_ID_CARLA_DEBUG" >> "$SCRIPT_DIR/.env"
 
 echo "CUDA_VERSION=$CUDA_VERSION" >> "$SCRIPT_DIR/.env"
