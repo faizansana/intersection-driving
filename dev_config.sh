9fbc9e7ce5a6ebdbaecc51d63a72a2459e3007ba
Faizan Sana <faizansana01@gmail.com>
2023-08-22 17:12:03 +0000

    Set carla server replicas using env vars

--- dev_config.sh
+++ dev_config.sh
@@ -28,10 +28,17 @@ GPU_ID_CARLA_MAIN=$(nvidia-smi --query-gpu=index,memory.used --format=csv,nohead
 # Find GPU with second lowest memory usage
 GPU_ID_CARLA_DEBUG=$(nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | sort -n -k2 | awk -F', ' '{print $1}' | head -n 2 | tail -n 1)
 
+
+## -------------------- CARLA Number of Replicas ------------------
+CARLA_SERVER_REPLICAS=${CARLA_SERVER_REPLICAS:-"5"}
+CARLA_DEBUG_SERVER_REPLICAS=${CARLA_DEBUG_SERVER_REPLICAS:-"1"}
+
+
 ## -------------------------- CUDA Version ------------------------
 CUDA_VERSION=${CUDA_VERSION:-"11.8.0"}
 
 
+## -------------------------- Write to .env -----------------------
 echo "FIXUID=$FIXUID" >> "$SCRIPT_DIR/.env"
 echo "FIXGID=$FIXGID" >> "$SCRIPT_DIR/.env"
 
@@ -40,5 +47,8 @@ echo "CARLA_QUALITY=$CARLA_QUALITY" >> "$SCRIPT_DIR/.env"
 
 echo "GPU_ID_CARLA_MAIN=$GPU_ID_CARLA_MAIN" >> "$SCRIPT_DIR/.env"
 echo "GPU_ID_CARLA_DEBUG=$GPU_ID_CARLA_DEBUG" >> "$SCRIPT_DIR/.env"
+
+echo "CARLA_SERVER_REPLICAS=$CARLA_SERVER_REPLICAS" >> "$SCRIPT_DIR/.env"
+echo "CARLA_DEBUG_SERVER_REPLICAS=$CARLA_DEBUG_SERVER_REPLICAS" >> "$SCRIPT_DIR/.env"
 
 echo "CUDA_VERSION=$CUDA_VERSION" >> "$SCRIPT_DIR/.env"
